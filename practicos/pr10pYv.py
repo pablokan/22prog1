e98535a8954d3bb241adc614093bb33dbd00ce67
Pablo Kaniefsky <pablokan@gmail.com>
2022-09-05 20:19:31 -0300

    pumasYvenados

--- practicos/pr10pYv.py
+++ practicos/pr10pYv.py
@@ -18,6 +18,12 @@ class Puma(Animal):
         else:
             return "enfermo"
 
+    def adulto(self):
+        if self.edad > 5:
+            return True
+        else:
+            return False
+
 class Venado(Animal):
         def __init__(self, id, peso) -> None:
             super().__init__(id, peso)
@@ -34,27 +40,45 @@ class Jaula:
         self.listaAnimales = []
         self.tipoAnimal = tipoAnimal
         for i in range(cantidad):
-            if tipoAnimal == "Puma":
-                a = Puma(i+1, input("Peso: "), input("Edad: "))
-            elif tipoAnimal == "Venado":
-                a = Venado(i+1, input("Peso: "))
+            if tipoAnimal == "Pumas":
+                caracP = [(230, 6), (180, 4), (210, 7), (190, 10)]
+                a = Puma(i+1, *caracP[i])
+            elif tipoAnimal == "Venados":
+                caracV = [100, 130]
+                a = Venado(i+1, caracV[i])
         
-        self.listaAnimales.append(a)
+            self.listaAnimales.append(a)
 
     def getData(self) -> str:
         c = 0
+        print(f"\nJaula de {self.tipoAnimal}")
         for animal in self.listaAnimales:
             print(f"# {animal.id} - {animal.salud()}")
-            if animal.edad > 5 and self.tipoAnimal == "Puma":
-                c += 1
-        if c != 0:
-            print(f"Cantidad de adultos: {c}")
+            
+    def getCantidadPumasAdultos(self):
+        c = 0
+        for puma in self.listaAnimales:
+            if puma.adulto(): 
+                    c += 1
+        print(f"Cantidad de adultos: {c}")
 
-jaulaPumas1 = Jaula("Puma", 3)
+jaulaPumas1 = Jaula("Pumas", 4)
 jaulaPumas1.getData()
+jaulaPumas1.getCantidadPumasAdultos()
 
-jaulaVenados1 = Jaula("Venado", 2)
+jaulaVenados1 = Jaula("Venados", 2)
 jaulaVenados1.getData()
 
-        
+# Salida
+# Jaula de Pumas
+# 1 - sano
+# 2 - enfermo
+# 3 - sano
+# 4 - enfermo
+# Cantidad de adultos: 3
+
+# Jaula de Venados
+# 1 - enfermo
+# 2 - sano
+
 
